d50fd7faabe5dc2ae81d4a1099ccad8c77cf37fe
Shankar Ram <ramshankar29@gmail.com>
2016-10-20 14:01:31 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -29,7 +29,7 @@ setwd('C:/Users/natarash/Desktop/R/R/UCI HAR Dataset/');
 features     = read.table('./features.txt',header=FALSE); #imports features.txt
 activityType = read.table('./activity_labels.txt',header=FALSE); #imports activity_labels.txt
 subjectTrain = read.table('./train/subject_train.txt',header=FALSE); #imports subject_train.txt
-xTrain       = read.table('./train/x_train.txt',header=FALSE); #imports x_train.txt
+xTrain       = read.table('./train/X_train.txt',header=FALSE); #imports x_train.txt
 yTrain       = read.table('./train/y_train.txt',header=FALSE); #imports y_train.txt
 
 # Assigin column names to the data imported above
@@ -43,7 +43,7 @@ trainingData = cbind(yTrain,subjectTrain,xTrain);
 
 # Read in the test data
 subjectTest = read.table('./test/subject_test.txt',header=FALSE); #imports subject_test.txt
-xTest       = read.table('./test/x_test.txt',header=FALSE); #imports x_test.txt
+xTest       = read.table('./X_test.txt',header=FALSE); #imports x_test.txt
 yTest       = read.table('./test/y_test.txt',header=FALSE); #imports y_test.txt
 
 # Assign column names to the test data imported above
